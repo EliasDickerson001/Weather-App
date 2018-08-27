0d3bed396b9577fe3ac34031798b8fd7bcc31d57
Elias Dickerson <eliasdickerson@gmail.com>
2018-08-27 16:48:54 -0500

    Add functionality for retrieving and organizing forecast data

--- src/Components/WeatherInterfaceContainer.js
+++ src/Components/WeatherInterfaceContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SearchForm} from './SearchForm';
 import {WeatherStats} from './WeatherStats';
+import {DailyWeatherStats} from '../Scripts/WeatherObjects';
 
 export class WeatherInterfaceContainer extends React.Component {
     constructor(props) {
@@ -12,14 +13,17 @@ export class WeatherInterfaceContainer extends React.Component {
         }
         this.getCurrWeatherData = this.getCurrWeatherData.bind(this);
         this.onInputChange = this.onInputChange.bind(this);
+        this.aggregateForecastData = this.aggregateForecastData.bind(this);
     }
     getCurrWeatherData(event){
         event.preventDefault();
         // To use application, obtain API key from OpenWeatherMap 
-        let apiKey = "***";
+        let apiKey = "5dc1a6d0183b60c9deeb6b1830b72d84";
+        //Fetch data for current city weather 
         let requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.currCity}&units=imperial&APPID=${apiKey}`;
         fetch(requestUrl).then(response => response.json())
         .then(json => {
+            console.log("Current JSON");
             console.log(json);
             //If code starts with 4, give alert
             if (json.cod[0] === "4"){
@@ -52,6 +56,49 @@ export class WeatherInterfaceContainer extends React.Component {
         ,networkError => {
             console.log(networkError);
         });
+        //Fetch data for forecasted city weather
+        requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${this.state.currCity}&units=imperial&APPID=${apiKey}`;
+        fetch(requestUrl).then(response => response.json())
+        .then(json => this.aggregateForecastData(json));
+    }
+    aggregateForecastData(json){
+        console.log("Forecast JSON");
+        console.log(json);
+        let forecastArray = json.list;
+        let forecastDayTime;
+        // 5 day forecast provided, stats for every 3 hours, day0 is today
+        let day0List = [], day1List = [], day2List = [], day3List = [], day4List = [], day5List = [];
+        forecastArray.forEach(forecastItem => {
+            // Date is Unix format returned in seconds, convert to miliseconds
+            forecastDayTime = new Date(forecastItem.dt*1000);
+            let day = DailyWeatherStats.getForecastPosition(forecastDayTime);
+            switch (day) {
+                case 0:
+                    day0List.push(forecastItem);
+                    break;
+                case 1:
+                    day1List.push(forecastItem);    
+                    break;
+                case 2:
+                    day2List.push(forecastItem);    
+                    break;
+                case 3:
+                    day3List.push(forecastItem);  
+                    break;
+                case 4:
+                    day4List.push(forecastItem);  
+                    break;
+                case 5:
+                    day5List.push(forecastItem);  
+                    break;
+            }
+        });
+        let day0 = day0List[0] ? new DailyWeatherStats(day0List) : null;
+        let day1 = day1List[0] ? new DailyWeatherStats(day1List) : null;
+        let day2 = day2List[0] ? new DailyWeatherStats(day2List) : null;
+        let day3 = day3List[0] ? new DailyWeatherStats(day3List) : null;
+        let day4 = day4List[0] ? new DailyWeatherStats(day4List) : null;
+        let day5 = day5List[0] ? new DailyWeatherStats(day5List) : null;
     }
     onInputChange(event){
         this.setState({
---
+++ src/Scripts/WeatherObjects.js
@@ -0,0 +1,112 @@
+// Weather information for a single day's weather
+export class DailyWeatherStats{
+    constructor(forecastArray){
+        this._forecast = forecastArray;
+    }
+    getHighTemp(){
+        let temp;
+        this._forecast.forEach((foreCastItem, index) => {
+            if (index === 0) {
+                temp = foreCastItem.main.temp;
+            } else {
+                if (foreCastItem.main.temp > temp) {
+                    temp = foreCastItem.main.temp;
+                }
+            }
+        });
+        return Math.round(temp);
+    }
+    getLowTemp(){
+        let temp;
+        this._forecast.forEach((foreCastItem, index) => {
+            if (index === 0) {
+                temp = foreCastItem.main.temp;
+            } else {
+                if (foreCastItem.main.temp < temp) {
+                    temp = foreCastItem.main.temp;
+                }
+            }
+        });
+        return Math.round(temp);
+    }
+    //Return day position of 5 day forecast
+    static getForecastPosition(forecastDate){
+        let currDayTime = new Date();
+        let day = currDayTime.getDay();
+        let fcDay = forecastDate.getDay();
+        let position = 0;
+        if (day === fcDay){
+            return position;
+        } else {
+            while (day !== fcDay){
+                if (day >= 6){
+                    day = 0;
+                    position++;
+                } else{
+                    day++;
+                    position++;
+                }
+            }
+        }
+        return position;
+    }
+    //Retrieves most prominent weather category for given day of 5 day forecast.
+    getProminentWeather(day){
+        let weatherCategories = {};
+        let catArray;
+        this._forecast.forEach((foreCastItem) => {
+            let forecastDate = new Date(foreCastItem.dt*1000);
+            if (DailyWeatherStats.getForecastPosition(forecastDate) === day) {
+                catArray = Object.keys(weatherCategories);
+                if (!catArray.includes(foreCastItem.weather[0].main)) {
+                    weatherCategories[foreCastItem.weather[0].main] = 1
+                } else {
+                    weatherCategories[foreCastItem.weather[0].main]++
+                }
+            }
+        });
+        let countArray = Object.values(weatherCategories);
+        let highestCount = Math.max(...countArray);
+        let returnArray = [];
+        weatherCategories  = Object.entries(weatherCategories);
+        weatherCategories.forEach((weather) => {
+            if (weather[1] === highestCount) {
+                returnArray.push(weather[0]);
+            }
+        });
+        return returnArray;
+    }
+    chanceOfRain(){
+        let chance = false;
+        this._forecast.forEach((foreCastItem) => {
+            //Weather prop in JSON returned as array, sometimes contains multiple entries.
+            foreCastItem.weather.forEach((weatherItem) => {
+                if (weatherItem.main === "Rain") {
+                    chance = true;
+                }
+            });
+        });
+        return chance;
+    }
+    getDayOfWeek(){
+        //Only need to check date in first array entry, forecast already categorized by date
+        let date = new Date(this._forecast[0].dt*1000);
+        let dayVal = date.getDay();
+        switch (dayVal) {
+            case 0:
+                return "Sunday";
+            case 1:    
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:    
+                return "Friday";
+            case 6:
+                return "Saturday";
+        }
+    }
+}
